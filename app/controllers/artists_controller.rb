f48c18b88ba7a6cb6b8a96ee0114dd8a1536c565
Drew Keat <thedrewkeat@gmail.com>
2021-07-22 11:35:19 -0600

    stub views for artist, genres, and songs

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,2 +1,7 @@
 class ArtistsController < ApplicationController
+
+    # navigate artist pages shows the name on the show page in a h1 tag
+    # navigate artist pages to artist pages
+    # navigate artist pages shows the bio on the show page in a p tag
+    
 end
